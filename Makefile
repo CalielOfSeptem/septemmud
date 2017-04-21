f356736312badcfa9ed72fbe4e0fb2a5f199137f
Ken Thompson <kenthompson1@gmail.com>
2017-04-21 09:43:49 -0400

    a new start

---
+++ Makefile
@@ -0,0 +1,8 @@
+.PHONY: clean All
+
+All:
+	@echo "----------Building project:[ septemserver - Debug ]----------"
+	@cd "septemserver" && "$(MAKE)" -f  "septemserver.mk" && "$(MAKE)" -f  "septemserver.mk" PostBuild
+clean:
+	@echo "----------Cleaning project:[ septemserver - Debug ]----------"
+	@cd "septemserver" && "$(MAKE)" -f  "septemserver.mk" clean
---
+++ septemserver/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/program_options.hpp>
+#include <thread>
+#include <boost/format.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+
+namespace ba = boost::asio;
+namespace po = boost::program_options;
+
+int main(int argc, char **argv)
+{
+    
+    boost::property_tree::ptree pt;
+    boost::property_tree::ini_parser::read_ini("config.ini", pt);
+    std::cout << pt.get<std::string>("options.port") << std::endl;
+
+    
+    unsigned int port        = 4000;
+    std::string  threads     = "";
+    unsigned int concurrency = 0;
+    
+    po::options_description description("Available options");
+    description.add_options()
+        ( "help,h",                                       "show this help message"                            )
+        ( "port,p",    po::value<unsigned int>(&port),    "port number"                                       )
+        ( "threads,t", po::value<std::string>(&threads),  "number of threads of execution (0 for autodetect)" )
+        ;
+
+    po::positional_options_description pos_description;
+    pos_description.add("port", -1);
+    try
+    {
+        po::variables_map vm;
+        po::store(
+            po::command_line_parser(argc, argv)
+                .options(description)
+                .positional(pos_description)
+                .run()
+          , vm);
+        
+        po::notify(vm);
+      
+        if (vm.count("help") != 0)
+        {
+            throw po::error("");
+        }
+        else if (vm.count("port") == 0)
+        {
+            throw po::error("Port number must be specified");
+        }
+
+        if (vm.count("threads") == 0)
+        {
+            concurrency = 1;
+        }
+        else
+        {
+            try
+            {
+                concurrency = boost::lexical_cast<unsigned int>(threads);
+            }
+            catch(...)
+            {
+                // Failure is to be expected here, since it might be an empty
+                // string.  In this case, concurrency will be a detectable 0.
+            }
+            
+            if (concurrency == 0)
+            {
+                concurrency = std::thread::hardware_concurrency();
+            }
+
+            // According to the Boost docs, "thread::hardware_concurrency()" 
+            // may return 0 on platforms that don't have information available
+            // about cores/hyperthreading units, etc.  In this case, we will
+            // default to one thread.
+            if (concurrency == 0)
+            {
+                concurrency = 1;
+            }
+        }
+    }
+    catch(po::error &err)
+    {
+        if (strlen(err.what()) == 0)
+        {
+            std::cout << boost::format("USAGE: %s <port number>|<options>\n")
+                        % argv[0]
+                 << description
+                 << std::endl;
+                 
+            return EXIT_SUCCESS;
+        }
+        else
+        {
+            std::cerr << boost::format("ERROR: %s\n\nUSAGE: %s <port number>|<options>\n")
+                        % err.what()
+                        % argv[0]
+                 << description
+                 << std::endl;
+        }
+        
+        return EXIT_FAILURE;
+    }
+	
+	return 0;
+}
